df7ec28b6421fe4e1064b70fb95e22680b2e10ea
Dan Reeves <heydanreeves@gmail.com>
2017-07-31 13:23:36 +0100

    funky;

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
-import styled from 'styled-components';
-import Layout from '../layouts/default.js';
-import Box from '../components/box.js';
+import Base from '../components/base.js';
+import { BaffledLink } from '../components/common/Link.js';
+import Link from 'next/link';
 
 export default () =>
-    <Layout>
-        <Box tall={2}>
+    <Base title="Dan Reeves">
+        <div>
             <p>
-                Hi. I'm Dan Reeves, lead developer at fffunction, a user-centred
-                design agency. I love writing JavaScript and Python. My other
-                passion is playing, making, and talking video games. I also
+                Hey. I'm <BaffledLink href="https://twitter.com/dnrvs">Dan Reeves</BaffledLink>,
+                Frontend Engineer at <BaffledLink href="https://treasuredata.com">Treasure Data</BaffledLink>.
+                I love all things JavaScript. Another passion is playing,
+                making, and talking video games. I also
                 co-organise Cornwall Geeks.
             </p>
-        </Box>
+        </div>
 
-        <Box wide={2}>
+        <div>
             <h2>Latest posts</h2>
             <ol>
                 <li><a href="#">Cornwall Geeks is 3!</a></li>
@@ -21,9 +22,9 @@ export default () =>
                 <li><a href="#">JavaScript isn’t just JavaScript</a></li>
             </ol>
             <a href="#">View all...</a>
-        </Box>
+        </div>
 
-        <Box>
+        <div>
             <h2>Glitch demos</h2>
             <ul>
                 <li><a href="#">Choodux</a></li>
@@ -34,9 +35,9 @@ export default () =>
                 <li><a href="#">http-referers</a></li>
             </ul>
             <a href="#">See more...</a>
-        </Box>
+        </div>
 
-        <Box>
+        <div>
             <h2>GitHub repos</h2>
             <ul>
                 <li><a href="#">Choodux</a></li>
@@ -47,16 +48,20 @@ export default () =>
                 <li><a href="#">http-referers</a></li>
             </ul>
             <a href="#">See more...</a>
-        </Box>
+        </div>
 
-        <Box>
+        <div>
             <h2>Bookmarks</h2>
             <ul>
                 <li><a href="#">How to deploy software</a></li>
                 <li><a href="#">How to build stable systems</a></li>
-                <li><a href="#">Engineering a culture of psychological safety</a></li>
+                <li>
+                    <a href="#">
+                        Engineering a culture of psychological safety
+                    </a>
+                </li>
             </ul>
             <a href="#">See more...</a>
-        </Box>
+        </div>
 
-    </Layout>;
+    </Base>;

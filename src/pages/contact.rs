9c3908ccc3d8061b093a2e510827d0f1c01b9fc5
Dan Reeves <hey@danreev.es>
2018-09-21 01:58:23 +0100

    this is going so nicely

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -1,17 +1,17 @@
 use actix_web::Path;
 use comrak::{markdown_to_html, ComrakOptions};
-use maud::{html, Markup, PreEscaped, DOCTYPE};
+use maud::{html, Markup, PreEscaped};
+use partials::page;
 
 const MD: &str = include_str!("contact.md");
 
 pub fn contact(_params: Path<()>) -> Markup {
-    let content: String = markdown_to_html(MD, &ComrakOptions::default());
+    let options = ComrakOptions {
+        ext_strikethrough: true,
+        ..ComrakOptions::default()
+    };
+    let content: String = markdown_to_html(MD, &options);
     html! {
-        (DOCTYPE)
-            html {
-                body style="font-family: monospace;" {
-                    (PreEscaped(content))
-                }
-            }
+        (page("Contact", PreEscaped(content)))
     }
 }
--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,11 +1,35 @@
 use actix_web::Path;
 use maud::{html, Markup};
+use partials::page;
+
+fn hello_planet() -> Markup {
+    html! {
+        @for (i, char) in "planet!".chars().enumerate() {
+            span.b[i % 2 == 0].g[i % 2 != 0] { (char) }
+        }
+    }
+}
 
 pub fn index(_params: Path<()>) -> Markup {
     html! {
-        div style="font-family: monospace;" {
-            h1 { "Hello, planet!" }
-            a href="/contact" { "Say hello!" }
-        }
+        (page("", html! {
+            h1 { "Hello, " (hello_planet()) }
+            p {
+                "I'm Dan Reeves, Frontend Engineer at "
+                a href="https://treasuredata.com" { "Arm" }
+                "."
+
+                r#" I like writing JavaScript & Rust.
+                You'll sometimes find me playing video games or reading books.
+                I also organise
+                "#
+                a href="https://join.cornwallgeeks.net" { "Cornwall Geeks" }
+                "."
+            }
+            p {
+                "Want to talk? "
+                a href="/contact" { "Get in touch!" }
+            }
+        }))
     }
 }

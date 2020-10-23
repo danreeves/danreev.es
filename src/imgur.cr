02b85896483281e40f5ba1c5d159fda1890a4006
Dan Reeves <hey@danreev.es>
2020-10-23 17:34:16 +0100

    New album

--- src/imgur.cr
+++ src/imgur.cr
@@ -9,6 +9,7 @@ module Imgur
   CLIENT    = HTTP::Client.new(URI.parse("https://api.imgur.com"))
   CACHE     = Cache::MemoryStore(String, AlbumResponse).new(expires_in: 24.hours)
   ALBUM_IDS = [
+    "wfI8IBa",
     "2by7snO",
     "O4loGdV",
   ]

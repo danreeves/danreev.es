1a738f6c501a4e2b981fe5d5a76698b9f9150a90
Dan Reeves <hey@danreev.es>
2022-11-19 15:44:54 +0000

    weather

--- app/about.tsx
+++ app/about.tsx
@@ -3,8 +3,12 @@ import { useData } from "./loader.tsx";
 
 export default function About() {
 	const [pokemon, setPokemon] = useState(270);
-	const [data, revalidate] = useData(
+	const [data] = useData(
 		`https://pokeapi.co/api/v2/pokemon/${pokemon}`,
+	);
+
+	const [weather, revalidate] = useData(
+		`https://coolweather.glitch.me/london`,
 	);
 
 	// console.log("RENDER", data);
@@ -14,21 +18,13 @@ export default function About() {
 			<h2>About</h2>
 			<h3>{data.name}</h3>
 			<img src={data.sprites.front_default} height="96" width="96" />
-			<img
-				src={data.sprites.front_default.replace(`${pokemon}`, `${pokemon + 1}`)}
-				style={{
-					height: 1,
-					width: 1,
-					opacity: 0,
-					pointerEvents: "none",
-					position: "fixed",
-					top: 0,
-					left: 0,
-				}}
-				aria-hidden
-			/>
 
-			<button onClick={() => revalidate()}>Refresh</button>
+			<PreloadImg
+				src={data.sprites.front_default.replace(`${pokemon}`, `${pokemon + 1}`)}
+			/>
+			<PreloadImg
+				src={data.sprites.front_default.replace(`${pokemon}`, `${pokemon - 1}`)}
+			/>
 
 			<button
 				onClick={() => {
@@ -48,6 +44,31 @@ export default function About() {
 			>
 				Next pokemon
 			</button>
+			<p>
+				<small>
+					<button onClick={() => revalidate()}>Refresh</button>{" "}
+					Weather in London: {weather.weather}, feels like {weather.feelsLike}
+					{"℃"}
+				</small>
+			</p>
 		</>
+	);
+}
+
+function PreloadImg({ src }: { src: string }) {
+	return (
+		<img
+			src={src}
+			style={{
+				height: 1,
+				width: 1,
+				opacity: 0,
+				pointerEvents: "none",
+				position: "fixed",
+				top: 0,
+				left: 0,
+			}}
+			aria-hidden
+		/>
 	);
 }

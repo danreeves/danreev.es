1f0e214b292d0ffecbd7f5a2c6ba4cd0815481b0
Dan Reeves <hey@danreev.es>
2022-11-19 02:28:30 +0000

    pokemon

--- app/about.tsx
+++ app/about.tsx
@@ -1,22 +1,46 @@
-import { use, useReducer } from "react";
-import { loader } from "./loader.tsx";
+import { startTransition, useState } from "react";
+import { useData } from "./loader.tsx";
 
 export default function About() {
-	const data = loader("https://coolweather.glitch.me/london");
-	const [, render] = useReducer((s) => s + 1, 0);
+	const [pokemon, setPokemon] = useState(270);
+	const [data] = useData(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+	// console.log("RENDER", data);
+
 	return (
 		<>
 			<h2>About</h2>
-			<p>hello</p>
-			<p>{data.weather}</p>
-			<p>{data.feelsLike}</p>
+			<h3>{data.name}</h3>
+			<img src={data.sprites.front_default} height="96" width="96" />
+			<img
+				src={data.sprites.front_default.replace(`${pokemon}`, `${pokemon + 1}`)}
+				style={{
+					height: 1,
+					width: 1,
+					opacity: 0,
+					pointerEvents: "none",
+					position: "fixed",
+					top: 0,
+					left: 0,
+				}}
+				aria-hidden
+			/>
 			<button
 				onClick={() => {
-					// map.delete("https://coolweather.glitch.me/london");
-					// render();
+					setPokemon((id) => id + 1);
 				}}
 			>
-				Refresh
+				Next pokemon
+			</button>
+
+			<button
+				onClick={() => {
+					startTransition(() => {
+						setPokemon((id) => id + 1);
+					});
+				}}
+			>
+				Next pokemon no loading
 			</button>
 		</>
 	);
--- app/loader.tsx
+++ app/loader.tsx
@@ -1,4 +1,12 @@
-import { createContext, ReactElement, use, useContext } from "react";
+import {
+	createContext,
+	ReactElement,
+	startTransition,
+	use,
+	useContext,
+	useReducer,
+} from "react";
+import isEqual from "https://esm.sh/v98/lodash.isequal@4.5.0/es2022/lodash.isequal.js";
 
 const LoaderContext = createContext<Map<string, unknown> | null>(null);
 
@@ -16,12 +24,39 @@ export function LoaderProvider({ children }: { children: ReactElement }) {
 	);
 }
 
-export function loader(url: string) {
+export function useData(url: string) {
 	const cache = useCache();
+	const [, render] = useReducer((s) => s + 1, 0);
 	const inCache = cache.get(url);
 
+	async function revalidate(showLoadingState = false) {
+		const prevData = await cache.get(url);
+
+		const promise = new Promise((resolve, reject) => {
+			fetch(url)
+				.then((res) => res.json())
+				.then((data) => {
+					resolve(data);
+					if (!isEqual(data, prevData)) {
+						render();
+					}
+				})
+				.catch((error) => {
+					reject(error);
+					render();
+				});
+		});
+
+		cache.set(url, promise);
+
+		if (showLoadingState) {
+			render();
+		}
+	}
+
 	if (inCache) {
-		return use(inCache);
+		const data = use(inCache);
+		return [data, revalidate];
 	}
 
 	const promise = new Promise((resolve, reject) => {
@@ -33,5 +68,37 @@ export function loader(url: string) {
 
 	cache.set(url, promise);
 
-	return use(promise);
+	const data = use(promise);
+
+	return [data, revalidate];
+}
+
+export function Img({ src, ...props }: React.DetailedHTMLProps<
+	React.ImgHTMLAttributes<HTMLImageElement>,
+	HTMLImageElement
+>) {
+	const img = loadImage(src);
+	console.log(img);
+	return <img src={img.src} {...props} />;
+}
+
+function loadImage(src: string | undefined) {
+	if (!src || !("Image" in window)) return;
+
+	const key = `IMG:${src}`;
+	const cache = useCache();
+	const inCache = cache.get(key);
+
+	if (inCache) {
+		return use(inCache);
+	} else {
+		const promise = new Promise((resolve, reject) => {
+			const img = new Image();
+			img.onload = () => resolve(img);
+			img.onerror = () => reject();
+			img.src = src;
+		});
+		cache.set(key, promise);
+		return use(promise);
+	}
 }

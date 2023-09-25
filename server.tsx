2f703f245b1f86cb9c553fc51727c03df9c290bd
Dan Reeves <hey@danreev.es>
2023-09-25 13:21:30 +0100

    try new font

--- server.tsx
+++ server.tsx
@@ -14,65 +14,63 @@ const router = new Router();
 router.add("GET", "/api/writing", getArticles);
 
 serve(async (request: Request): Promise<Response> => {
-	const method = request.method;
-	const preferredLang = getPreferredLang(request);
-	const { pathname: path } = new URL(request.url);
+  const method = request.method;
+  const preferredLang = getPreferredLang(request);
+  const { pathname: path } = new URL(request.url);
 
-	console.log(method, path, preferredLang);
+  console.log(method, path, preferredLang);
 
-	if (path.startsWith("/api")) {
-		return await router.route(request);
-	}
+  if (path.startsWith("/api")) {
+    return await router.route(request);
+  }
 
-	if (path.endsWith(".tsx") || path.endsWith(".ts")) {
-		return jsResponse(path);
-	}
+  if (path.endsWith(".tsx") || path.endsWith(".ts")) {
+    return jsResponse(path);
+  }
 
-	if (path.endsWith(".css")) {
-		return cssResponse(path);
-	}
+  if (path.endsWith(".css")) {
+    return cssResponse(path);
+  }
 
-	if (path.endsWith(".svg")) {
-		return svgResponse(path);
-	}
+  if (path.endsWith(".svg")) {
+    return svgResponse(path);
+  }
 
-	const importMap = await Deno.readTextFile("./importMap.json");
-	const clientScript = await Deno.readTextFile("./client.tsx");
+  const importMap = await Deno.readTextFile("./importMap.json");
+  const clientScript = await Deno.readTextFile("./client.tsx");
 
-	const stream = await ReactDOMServer.renderToReadableStream(
-		<html>
-			<title>hello, planet.</title>
-			<link rel="stylesheet" href="/styles.css" />
-			<script
-				dangerouslySetInnerHTML={{
-					__html: `console.time("❏ running client");`,
-				}}
-			/>
-			<script
-				type="importmap"
-				dangerouslySetInnerHTML={{ __html: importMap }}
-			/>
-			<script
-				type="module"
-				dangerouslySetInnerHTML={{ __html: clientScript }}
-			/>
-			<div id="app">
-				<ServerRoute.Provider value={path}>
-					<LangProvider lang={preferredLang}>
-						<App />
-					</LangProvider>
-				</ServerRoute.Provider>
-			</div>
-		</html>,
-	);
+  const stream = await ReactDOMServer.renderToReadableStream(
+    <html>
+      <title>hello, planet.</title>
+      <link rel="stylesheet" href="/styles.css" />
+      <link rel="stylesheet" href="https://use.typekit.net/wgj0akb.css" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `console.time("❏ running client");`,
+        }}
+      />
+      <script
+        type="importmap"
+        dangerouslySetInnerHTML={{ __html: importMap }}
+      />
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{ __html: clientScript }}
+      />
+      <div id="app">
+        <ServerRoute.Provider value={path}>
+          <LangProvider lang={preferredLang}>
+            <App />
+          </LangProvider>
+        </ServerRoute.Provider>
+      </div>
+    </html>
+  );
 
-	// Wait for suspense boundaries to settle
-	await stream.allReady;
+  // Wait for suspense boundaries to settle
+  await stream.allReady;
 
-	return new Response(
-		stream,
-		{
-			headers: { "Content-Type": "text/html;charset=utf-8" },
-		},
-	);
+  return new Response(stream, {
+    headers: { "Content-Type": "text/html;charset=utf-8" },
+  });
 });

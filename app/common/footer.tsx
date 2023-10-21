6d7ffff5e2407094631d61731680a593786ad23c
Dan Reeves <hey@danreev.es>
2023-10-21 14:45:59 +0100

    style changes

--- app/common/footer.tsx
+++ app/common/footer.tsx
@@ -1,71 +1,49 @@
+const SQUIGS = Array.from({ length: 4 }).map((_, i) => i);
+
+const style = `
+@keyframes squiggly-anim {
+  ${SQUIGS.map((i) => {
+    const p = (i / (SQUIGS.length - 1)) * 100;
+
+    return `${p}% {
+      filter: url(\"#squiggly-${i}\");  
+    }
+    `;
+  }).join("")}
+}
+`;
+
 export function Footer() {
-	return (
-		<>
-			<footer>
-				<small>this website was made by dan reeves. don't steal it.</small>
-			</footer>
+  return (
+    <>
+      <footer>
+        <small>this website was made by dan reeves :)</small>
+      </footer>
 
-			<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-				<defs>
-					<filter id="squiggly-0">
-						<feTurbulence
-							id="turbulence"
-							baseFrequency="0.02"
-							numOctaves="3"
-							result="noise"
-							seed="0"
-						/>
-						<feDisplacementMap
-							id="displacement"
-							in="SourceGraphic"
-							in2="noise"
-							scale="1"
-						/>
-					</filter>
-					<filter id="squiggly-1">
-						<feTurbulence
-							id="turbulence"
-							baseFrequency="0.02"
-							numOctaves="3"
-							result="noise"
-							seed="1"
-						/>
-						<feDisplacementMap in="SourceGraphic" in2="noise" scale="1" />
-					</filter>
-
-					<filter id="squiggly-2">
-						<feTurbulence
-							id="turbulence"
-							baseFrequency="0.02"
-							numOctaves="3"
-							result="noise"
-							seed="2"
-						/>
-						<feDisplacementMap in="SourceGraphic" in2="noise" scale="1" />
-					</filter>
-					<filter id="squiggly-3">
-						<feTurbulence
-							id="turbulence"
-							baseFrequency="0.02"
-							numOctaves="3"
-							result="noise"
-							seed="3"
-						/>
-						<feDisplacementMap in="SourceGraphic" in2="noise" scale="1" />
-					</filter>
-
-					<filter id="squiggly-4">
-						<feTurbulence
-							id="turbulence"
-							baseFrequency="0.02"
-							numOctaves="3"
-							result="noise"
-							seed="4"
-						/>
-						<feDisplacementMap in="SourceGraphic" in2="noise" scale="1" />
-					</filter>
-				</defs>
-			</svg>
-		</>
-	);
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          {SQUIGS.map((i) => {
+            return (
+              <filter id={`squiggly-${i}`}>
+                <feTurbulence
+                  id="turbulence"
+                  baseFrequency="0.02"
+                  numOctaves="3"
+                  result="noise"
+                  seed={i}
+                />
+                <feDisplacementMap
+                  id="displacement"
+                  in="SourceGraphic"
+                  in2="noise"
+                  scale="1"
+                />
+              </filter>
+            );
+          })}
+        </defs>
+      </svg>
+      <style dangerouslySetInnerHTML={{ __html: style }} />
+    </>
+  );
 }
--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -2,73 +2,70 @@ import { useData } from "../loader.tsx";
 import { Link } from "../common/router.tsx";
 
 const inlineImageStyles = {
-	display: "inline",
-	height: "1.5em",
-	width: "1.5em",
-	verticalAlign: "middle",
+  display: "inline",
+  height: "1.5em",
+  width: "1.5em",
+  verticalAlign: "middle",
 };
 
 export default function About() {
-	const [weather] = useData(
-		`https://coolweather.glitch.me/london`,
-	);
+  const [weather] = useData(`https://coolweather.glitch.me/london`);
 
-	return (
-		<>
-			<h2>About</h2>
-			<p>
-				I’m Dan Reeves, Tech Lead on the Frontend team at{" "}
-				<Link to="https://treasuredata.com">Treasure Data</Link>. My day job is
-				mostly React &amp; TypeScript but I get up to a lot else.
-			</p>
-			<p>
-				I live in London but I've worked fully async & remote since 2017.
-			</p>
-			<p>
-				This site is currently a server side rendering react site with suspense
-				data loading{" "}
-				<Link to="https://github.com/danreeves/danreev.es">
-					written from scratch
-				</Link>{" "}
-				on Deno.{" "}
-				<img
-					src="/img/deno.svg"
-					style={{ ...inlineImageStyles, "verticalAlign": "bottom" }}
-				/>
-			</p>
-			<p>
-				I've been heavily involved in the{" "}
-				<Link to="https://vmf-docs.verminti.de/">
-					Vermintide Modding Community
-				</Link>{" "}
-				where I write a lot of{" "}
-				<Link to="https://github.com/danreeves/vermintide-2-mods">lua</Link>.
-				For a while I regularly livestreamed.
-			</p>
-			<p>
-				Before that still, I worked on fullstack builds with php (WordPress) and
-				python (Django), prototyping, ux testing, and much more for all variety
-				of clients at <Link to="https://fffunction.co">fffunction</Link>.
-			</p>
-			<p>
-				Outside of programming, I enjoy film photography. You can see my work on
-				{" "}
-				<Link to="https://instagram.com/dnrvs">Instagram</Link>.
-			</p>
-			<p className="box left">
-				<img
-					style={inlineImageStyles}
-					src={`https://openweathermap.org/img/wn/${weather._meta.icon}@2x.png`}
-				/>{" "}
-				Weather in London: {weather.weather}, feels like{" "}
-				{Math.round(weather.feelsLike)}
-				{"°C"}.{" "}
-			</p>
-			<a className="box right" href="writing">See latest post {">"}</a>
-			<img
-				src="https://ghchart.rshah.org/c58fff/danreeves"
-				style={{ width: "100%", marginTop: "1em" }}
-			/>
-		</>
-	);
+  return (
+    <>
+      <h2>About</h2>
+      <p>
+        I’m Dan Reeves, Tech Lead on the Frontend team at{" "}
+        <Link to="https://treasuredata.com">Treasure Data</Link>. My day job is
+        mostly React &amp; TypeScript but I get up to a lot else.
+      </p>
+      <p>I live in London but I've worked fully async & remote since 2017.</p>
+      <p>
+        This site is currently a server side rendering react site with suspense
+        data loading{" "}
+        <Link to="https://github.com/danreeves/danreev.es">
+          written from scratch
+        </Link>{" "}
+        on Deno.{" "}
+        <img
+          src="/img/deno.svg"
+          style={{ ...inlineImageStyles, verticalAlign: "bottom" }}
+        />
+      </p>
+      <p>
+        I've been heavily involved in the{" "}
+        <Link to="https://vmf-docs.verminti.de/">
+          Vermintide Modding Community
+        </Link>{" "}
+        where I write a lot of{" "}
+        <Link to="https://github.com/danreeves/vermintide-2-mods">lua</Link>.
+        For a while I regularly livestreamed.
+      </p>
+      <p>
+        Before that still, I worked on fullstack builds with php (WordPress) and
+        python (Django), prototyping, ux testing, and much more for all variety
+        of clients at <Link to="https://fffunction.co">fffunction</Link>.
+      </p>
+      <p>
+        Outside of programming, I enjoy film photography. You can see my work on{" "}
+        <Link to="https://instagram.com/dnrvs">Instagram</Link>.
+      </p>
+      <p className="box left">
+        <img
+          style={inlineImageStyles}
+          src={`https://openweathermap.org/img/wn/${weather._meta.icon}@2x.png`}
+        />{" "}
+        Weather in London: {weather.weather}, feels like{" "}
+        {Math.round(weather.feelsLike)}
+        {"°C"}.{" "}
+      </p>
+      <a className="box right" href="writing">
+        See latest post {">"}
+      </a>
+      <img
+        src="https://ghchart.rshah.org/danreeves"
+        style={{ width: "100%", marginTop: "1em" }}
+      />
+    </>
+  );
 }

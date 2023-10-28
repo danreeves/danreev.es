9e5c2cffc647acb266391399c1a2d5f9c11abd94
Dan Reeves <hey@danreev.es>
2023-10-28 04:46:43 +0100

    wrong syntax

--- src/partials.cr
+++ src/partials.cr
@@ -8,7 +8,7 @@ def page_head(page_title : String)
     meta({name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no"}),
     link({rel: "icon", href: "/favicon.ico"}),
     link({rel: "apple-touch-icon", href: "/favicon.png"}),
-    link({rel="stylesheet", href="https://use.typekit.net/wgj0akb.css"}),
+    link({rel: "stylesheet", href:"https://use.typekit.net/wgj0akb.css"}),
     style("
       * { font-family: \"felt-tip-woman\"; }
       html {

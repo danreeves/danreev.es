781a5d7857a8df02186711c0b423deb999c1e4f2
Dan Reeves <hey@danreev.es>
2023-10-30 21:41:03 +0000

    dont capitalize all h1s

--- src/partials.cr
+++ src/partials.cr
@@ -21,7 +21,7 @@ def page_head(page_title : String)
       }
       body { max-width: 95%; width: 45ch; margin-left: auto; margin-right: auto; }
       img { max-width: 100%; }
-      h1 { font-weight: 900; font-size: 3rem; line-height: 2.3rem; text-transform: capitalize; }
+      h1 { font-weight: 900; font-size: 3rem; line-height: 2.3rem; }
       pre { background: #fff; padding: 1rem; overflow-x: scroll; }
       dd { margin-top: 1rem; margin-bottom: 1rem; }
       code { background: #fff; font-family: monospace; font-size: .75em; }

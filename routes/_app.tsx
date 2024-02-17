2a60035c4bfdc328dae50d9086e4a49e6a9380f0
Dan Reeves <hey@danreev.es>
2024-02-17 17:48:48 +0000

    syntax highlight code

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,9 @@
 import { type PageProps } from "$fresh/server.ts";
+import css from "npm:nue-glow/minified/glow.css";
+
+const glowCss = Deno.readTextFileSync(
+  "./node_modules/nue-glow/minified/glow.css"
+);
 
 export default function App({ Component }: PageProps) {
   return (
@@ -15,7 +20,6 @@ export default function App({ Component }: PageProps) {
         <link rel="stylesheet" href="https://use.typekit.net/wgj0akb.css" />
         <style>
           {`
-      * { font-family: 'felt-tip-woman'; }
       html {
         font-size: 2rem;
         margin: 1rem;
@@ -23,17 +27,21 @@ export default function App({ Component }: PageProps) {
         background-image: radial-gradient(#c1c1c1 1px, transparent 0);
         background-size: 40px 40px;
         background-position: -19px -19px;
+        font-family: 'felt-tip-woman';
       }
       body { max-width: 95%; width: 45ch; margin-left: auto; margin-right: auto; }
       img { max-width: 100%; }
       h1 { font-weight: 900; font-size: 3rem; line-height: 2.3rem; }
-      pre { background: #fff; padding: 1rem; overflow-x: scroll; }
+      pre { font-family: monospace !important; font-size: 0.6em; background: white;}
       dd { margin-top: 1rem; margin-bottom: 1rem; }
-      code { background: #fff; font-family: monospace; font-size: .75em; }
       a { color: #000; }
       .js a { text-decoration: none; }
+      [glow] {
+  --glow-padding: .5em;
+}
       `}
         </style>
+        <style>{glowCss}</style>
         <script type="module" src="/script.js" />
       </head>
       <body>
--- utils/md.ts
+++ utils/md.ts
@@ -4,10 +4,15 @@ import { slugify } from "$slugify";
 import { extract } from "https://deno.land/std@0.215.0/front_matter/any.ts";
 import { test } from "https://deno.land/std@0.215.0/front_matter/test.ts";
 import * as valibot from "$valibot";
+import { glow } from "npm:nue-glow";
 
 class CustomRenderer extends Renderer {
   heading(text: string, level: 1 | 2 | 3 | 4 | 5 | 6): string {
     return `<h${level} id="${slugify(text)}">${text}</h${level}>`;
+  }
+  code(code: string, language?: string) {
+    const html = glow(code, { language, numbered: true });
+    return `<pre glow>${html}</pre>`;
   }
 }
 
@@ -20,11 +25,8 @@ export function render(mdString: string) {
 
 export function frontMatter<
   S extends valibot.BaseSchema,
-  T = valibot.Output<S>,
->(
-  mdString: string,
-  schema: S,
-): { title: string; body: string; attrs: T } {
+  T = valibot.Output<S>
+>(mdString: string, schema: S): { title: string; body: string; attrs: T } {
   if (test(mdString)) {
     const { body, attrs: unknownAttrs } = extract(mdString);
     const title = body.match(/^# (.*)/)?.[1] ?? "Untitled";
